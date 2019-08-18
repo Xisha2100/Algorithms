13e0f25d2bc8945644a9a5a2b6f286d022c0b722
Noah <32444724+Xisha2100@users.noreply.github.com>
2019-08-18 23:12:30 +0800

    Create 1_1_5.java

---
+++ 1基础/1.1基础编程模型/1_1_5.java
@@ -0,0 +1,24 @@
+package ty_pack;
+import edu.princeton.cs.algs4.*; 
+
+public class TY_class1
+{
+	public static void main(String[] args) 
+	{
+		double x, y;
+		StdOut.println("Input x, y");
+		x=StdIn.readDouble();
+		y=StdIn.readDouble();
+		if(between_num(x) &&between_num(y))
+			StdOut.println("between");
+		else
+			StdOut.println("not between");
+	}
+	public static boolean between_num(double temp)
+	{
+		if(temp<1.0 && temp>0.0)
+			return true;
+		else
+			return false;
+	}
+}
