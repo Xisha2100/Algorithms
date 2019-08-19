b2a2c064c66f9b76d5e89bdff91f318f8d7348f4
Noah <32444724+Xisha2100@users.noreply.github.com>
2019-08-19 22:32:04 +0800

    Create 1_1_9.java

---
+++ 1基础/1.1基础编程模型/1_1_9.java
@@ -0,0 +1,20 @@
+package ty_pack;
+import edu.princeton.cs.algs4.*; 
+
+public class TY_class1
+{
+	public static void main(String[] args) 
+	{
+		int n = 0;
+		n=StdIn.readInt();		
+		StdOut.println(d2bs(n));
+	}
+	
+	public static String d2bs(int n)
+	{
+		String s="";
+		for(;n>0;n/=2)
+			s=(n%2)+s;
+		return s;
+	}
+}
