e1a4643f21d954f806413f8602640fc83f1f86b6
Noah <32444724+Xisha2100@users.noreply.github.com>
2019-08-21 00:06:27 +0800

    1.1.13

---
+++ 1基础/1.1基础编程模型/1_1_13.java
@@ -0,0 +1,36 @@
+package ty_pack;
+import edu.princeton.cs.algs4.*; 
+
+public class TY_class1
+{
+	public static void main(String[] args) 
+	{
+		int[][] a= new int[12][6];
+		int[][] b= new int[6][12];
+		for(int i=0; i<12; i++)
+			for(int j=0; j<6; j++)
+				a[i][j]=StdRandom.uniform(9999);
+		b=exchange(a);
+		print(a);
+		print(b);
+	}
+	
+	public static int[][] exchange(int[][] a)
+	{
+		int[][] b= new int[a[0].length][a.length];
+		for(int i=0; i<a.length; i++)
+			for(int j=0; j<a[0].length; j++)
+				b[j][i]=a[i][j];
+		return b;
+	}
+	
+	public static void print(int[][] a)
+	{
+		for(int i=0; i<a.length; i++)
+		{
+			for(int j=0; j<a[0].length; j++)
+				StdOut.printf("%4d ",a[i][j]);
+			StdOut.println();
+		}
+	}	
+}
