f98a1bdef769dbb0491b9c2918c42c0728db8d50
Noah <32444724+Xisha2100@users.noreply.github.com>
2019-08-21 12:05:20 +0800

    1.1.15

---
+++ 1基础/1.1基础编程模型/1_1_15.java
@@ -0,0 +1,28 @@
+package ty_pack;
+import edu.princeton.cs.algs4.*; 
+
+public class TY_class1
+{
+	public static void main(String[] args) 
+	{
+		int m=5; 
+		int[] a=new int[300];
+		int[] b=new int[m];
+		for(int i=0; i<300; i++)
+			a[i]=StdRandom.uniform(m);
+		b=histogram(a, m);
+		for(int i=0;i<5;i++)
+			StdOut.printf("%3d",b[i]);
+	}
+	
+	public static int[] histogram(int[] a, int m)
+	{
+		int[] b= new int[m];
+		for(int i=0; i<a.length;i++)
+		{
+			if(a[i]<m)
+				b[a[i]]+=1;
+		}
+		return b;
+	}	
+}
