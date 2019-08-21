ea7c65fcd63da66b7a36e1b65b1ce497ac85aa2b
Noah <32444724+Xisha2100@users.noreply.github.com>
2019-08-21 23:18:19 +0800

    1.1.20

---
+++ 1基础/1.1基础编程模型/EX1_1_20.java
@@ -0,0 +1,22 @@
+package exercise;
+import edu.princeton.cs.algs4.*;
+
+public class EX1_1_20
+{
+
+	public static void main(String[] args) 
+	{
+		int N=3;
+		StdOut.println(lge(N));
+		
+	} 
+
+	public static double lge(int n)
+	{
+		if(n>1)
+			return Math.log(n) + lge(n-1);
+		else
+			return 0;
+	}
+
+}
