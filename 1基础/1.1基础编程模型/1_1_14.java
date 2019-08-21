fe8067ec77bbd846576589558386f63be8b6b3f1
Noah <32444724+Xisha2100@users.noreply.github.com>
2019-08-21 11:31:16 +0800

    1.1.14

---
+++ 1基础/1.1基础编程模型/1_1_14.java
@@ -0,0 +1,26 @@
+package ty_pack;
+import edu.princeton.cs.algs4.*; 
+
+public class TY_class1
+{
+	public static void main(String[] args) 
+	{
+		int n;
+		StdOut.println("input a num");
+		n=StdIn.readInt();
+		if(lg(n)==-1)
+			StdOut.println("wrong filed");
+		else
+			StdOut.println(lg(n));
+	}
+	
+	public static int lg(int N)
+	{
+		int i=-1;
+		for(; N>0;i++)
+		{
+			N/=2;
+		}
+		return i;
+	}	
+}
