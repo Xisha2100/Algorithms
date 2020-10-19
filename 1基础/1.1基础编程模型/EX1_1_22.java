68632cf935f1d62526999fdb210d832cc418a5df
Noah <32444724+Xisha2100@users.noreply.github.com>
2020-10-19 23:55:05 +0800

    Update EX1_1_22.java
    修改版本

--- 1基础/1.1基础编程模型/EX1_1_22.java
+++ 1基础/1.1基础编程模型/EX1_1_22.java
@@ -1,34 +1,35 @@
-package exercise;
-import edu.princeton.cs.algs4.*;
-
-public class EX1_1_22
+public class Ex1_1_22
 {
+    public static int rank(int key, int[] a) {
+        return rank(key, a, 0, a.length - 1, 0);
+    }
 
-	public static void main(String[] args) 
-	{
-		int[] a = {0,1,2,3,4,5,6,7,8,9};
-		StdOut.print(rank(9,a));
-	} 
-	
-	public static int rank(int key, int[] a)
-	{
-		return rank(key, a, 0, a.length-1, 0);
-	}
-	
-	public static int rank(int key, int[] a, int lo, int hi, int n)
-	{
-		if(lo>hi)
-			return -1;
-		int mid = lo + (hi - lo) / 2;
-		for(int i=0; i<n; i++)
-			StdOut.print("	");
-		StdOut.printf("%d\t%d\t\n", lo, hi);
-		if(key < a[mid])
-			return rank(key, a, 0, mid - 1, n+1);
-		else if(key > a[mid])
-			return rank(key, a, mid + 1, hi, n+1);
-		else
-			return mid;
-	}
+    public static int rank(int key, int[] a, int lo, int hi, int depth) {
+        for (int i = 0; i < depth; i++) {
+            System.out.printf("  ");
+        }
+        System.out.println("lo=" + lo + ", hi=" + hi);
+        if (lo > hi) {
+            return -1;
+        }
+        int mid = lo + (hi - lo) / 2;
+        if (key < a[mid]) {
+            return rank(key, a, lo, mid - 1, depth + 1);
+        } else if (key > a[mid]) {
+            return rank(key, a, mid + 1, hi, depth + 1);
+        } else {
+            return mid;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 12 };
+        int key = 12;
+        int pos = rank(key, a);
+        if (pos == -1) {
+            System.out.println("not found!");
+        } else {
+            System.out.println("position=" + pos);
+        }
+    }
 }
-//
