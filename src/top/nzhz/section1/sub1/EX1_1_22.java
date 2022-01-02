951f561e076a0b1f6093f5291604f09746996a87
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2022-01-03 01:13:36 +0800

    更新库文件，添加测试数据集

--- src/top/nzhz/section1/sub1/EX1_1_22.java
+++ src/top/nzhz/section1/sub1/EX1_1_22.java
@@ -6,22 +6,16 @@ public class EX1_1_22
         return rank(key, a, 0, a.length - 1, 0);
     }
 
-    public static int rank(int key, int[] a, int lo, int hi, int depth) {
-        for (int i = 0; i < depth; i++) {
-            System.out.printf("  ");
+    public static int rank(int key, int[] a, int lo, int hi,int dep) {
+        if (lo > hi) return -1;
+        for (int i = 0; i < dep; i++) {
+            System.out.printf(" ");
         }
-        System.out.println("lo=" + lo + ", hi=" + hi);
-        if (lo > hi) {
-            return -1;
-        }
+        System.out.printf("%d %d \n",lo,hi);
         int mid = lo + (hi - lo) / 2;
-        if (key < a[mid]) {
-            return rank(key, a, lo, mid - 1, depth + 1);
-        } else if (key > a[mid]) {
-            return rank(key, a, mid + 1, hi, depth + 1);
-        } else {
-            return mid;
-        }
+        if (key < a[mid]) return rank(key, a, lo, mid - 1,dep+1);
+        else if (key > a[mid]) return rank(key, a, mid + 1, hi,dep+1);
+        else return mid;
     }
 
     public static void main(String[] args) {
